df983c80039a36dd064454091bf7b13d20d8e46c
Franco Victorio <victorio.franco@gmail.com>
2020-05-15 11:16:47 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+type RemoteStatusSuccess<T> = { _type: 'success'; data: T }
+type RemoteStatus<T> =
+  | { _type: 'notAsked' }
+  | RemoteStatusSuccess<T>
+  | { _type: 'loading' }
+  | { _type: 'failure'; error: Error }
+
+function success<T>(data: T): RemoteStatusSuccess<T> {
+  return {
+    _type: 'success',
+    data,
+  }
+}
+
+function loading<T>(): RemoteStatus<T> {
+  return {
+    _type: 'loading',
+  }
+}
+
+function notAsked<T>(): RemoteStatus<T> {
+  return {
+    _type: 'notAsked',
+  }
+}
+
+export class Remote<T> {
+  protected status: RemoteStatus<T>
+
+  protected constructor(status?: RemoteStatus<T>) {
+    if (status) {
+      this.status = status
+    } else {
+      this.status = notAsked()
+    }
+  }
+
+  public hasData(): this is RemoteWithData<T> {
+    return this.status._type === 'success'
+  }
+
+  public match<U>({
+    onSuccess,
+    onLoading,
+    onNotAsked,
+    onFailure,
+  }: {
+    onSuccess: ((data: T) => U) | U
+    onLoading: (() => U) | U
+    onNotAsked: (() => U) | U
+    onFailure: ((e: Error) => U) | U
+  }): U {
+    if (this.status._type === 'notAsked') {
+      return onNotAsked instanceof Function ? onNotAsked() : onNotAsked
+    }
+    if (this.status._type === 'success') {
+      return onSuccess instanceof Function
+        ? onSuccess(this.status.data)
+        : onSuccess
+    }
+    if (this.status._type === 'loading') {
+      return onLoading instanceof Function ? onLoading() : onLoading
+    }
+    if (this.status._type === 'failure') {
+      return onFailure instanceof Function
+        ? onFailure(this.status.error)
+        : onFailure
+    }
+
+    const exhaustiveCheck: never = this.status
+    return exhaustiveCheck
+  }
+
+  public get(): T | null {
+    if (this.status._type === 'success') {
+      return this.status.data
+    }
+
+    return null
+  }
+
+  public isLoading(): boolean {
+    return this.status._type === 'loading'
+  }
+
+  public getOr(defaultData: T): T {
+    if (this.status._type === 'success') {
+      return this.status.data
+    }
+
+    return defaultData
+  }
+
+  static success<T>(data: T): Remote<T> {
+    return new Remote(success(data))
+  }
+
+  static notAsked<T>(): Remote<T> {
+    return new Remote()
+  }
+
+  static loading<T>(): Remote<T> {
+    return new Remote(loading())
+  }
+}
+
+class RemoteWithData<T> extends Remote<T> {
+  protected status: RemoteStatusSuccess<T>
+  constructor(data: T) {
+    super()
+    this.status = success(data)
+  }
+
+  public get(): T {
+    return this.status.data
+  }
+}
